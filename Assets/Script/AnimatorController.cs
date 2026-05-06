15934362a0ce2e705efa9eb6e4930e346f1030f8
Kime-MangaX <kimemanga@gmail.com>
2026-05-06 16:32:57 -0500

    Animation

---
+++ Assets/Script/AnimatorController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+
+    public InputSystem_Actions inputs;
+    public void Awake()
+    {
+        
+    }
+}
---
+++ Assets/Script/CharacterController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+
+
+public class CharacterController : BaseEntity, IDamageable
+{
+
+
+    public void TakeDamage()
+    {
+
+    }
+
+    public override void TakeDamage(BaseEntity damager, Elements elements)
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<ICollectable>() != null)
+        {
+            collision.gameObject.GetComponent<ICollectable>().Collect();
+        }
+
+    }
+}
