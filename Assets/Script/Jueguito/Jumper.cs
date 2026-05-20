1dd7987a87d82552c67f2710b3105ef30147321a
Kime-MangaX <kimemanga@gmail.com>
2026-05-20 14:48:44 -0500

    Jump para Trampolin

---
+++ Assets/Script/Jueguito/Jumper.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+
+    public float jumperForce;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumperForce, ForceMode2D.Impulse);
+        }
+    }
+}
